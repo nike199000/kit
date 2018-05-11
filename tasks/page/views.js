f0797a1b001197ba78ad839dd64a9f1beeaa462f
Idan Goldman <idan1986@gmail.com>
2018-05-11 17:00:57 +0300

    update version from package.json

--- tasks/page/views.js
+++ tasks/page/views.js
@@ -1,6 +1,7 @@
 const views = ({ gulp, plugins, done }) => {
     gulp.src('page/views/index.njk')
       .pipe( plugins.nunjucks.compile() )
+      .pipe( plugins.injectVersion({ prepend: '' }) )
       .pipe( plugins.rename('index.html') )
       .pipe( gulp.dest( 'build' ) )
       .on( 'end', done );
