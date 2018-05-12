556b559c9a270b3b3aeb0fbb72a09984c154e4c9
Idan Goldman <idan1986@gmail.com>
2018-05-12 20:20:48 +0300

    fixes and move of .main class to it's own file

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,6 +1,6 @@
 const watch = ({ gulp, plugins, done }) => {
-  gulp.watch( 'src/styles/**/*.scss', gulp.series('styles') );
-  gulp.watch( 'page/styles/**/*.scss', gulp.series('page-styles') );
+  // gulp.watch( 'src/styles/**/*.scss', gulp.series('styles') );
+  gulp.watch( ['src/styles/**/*.scss', 'page/styles/**/*.scss'], gulp.series('page-styles') );
   gulp.watch( 'page/views/**/*.njk', gulp.series('page-views') );
 
   done();
